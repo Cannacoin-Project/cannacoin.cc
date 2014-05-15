a119daa7afe71823a5d4abc8391880a7d72369f1
The Cannacoin Project <team@cannacoin.cc>
2014-05-14 18:44:00 -0700

    Update header.php

--- header.php
+++ header.php
@@ -12,16 +12,6 @@
     <link href='http://cannacoin.cc/bootstrap/css/bootstrap.css' rel='stylesheet'>
     <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
       
-    <!-- Google Analytics -->  
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-      ga('create', 'UA-48435154-1', 'cannacoin.cc');
-      ga('send', 'pageview');
-    </script>
-    <!-- End of Google Analytics -->  
     <!-- Custom styles for this template -->
     <link href='css/carousel.css' rel='stylesheet'>
     <link href='css/sticky-footer.css' rel='stylesheet'>
